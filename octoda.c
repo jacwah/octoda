253b7227ae113be72ed90beb061e88c918690baf
Jacob Wahlgren <jacob.wahlgren@gmail.com>
2015-07-19 18:39:31 +0200

    Add GPL v2 license

--- octoda.c
+++ octoda.c
@@ -1,3 +1,17 @@
+/* octoda - a disassembler for CHIP-8 programs
+ * Copyright 2015 Jacob Wahlgren
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+
 #include <stdio.h>
 #include <stdint.h>
 #include <errno.h>
